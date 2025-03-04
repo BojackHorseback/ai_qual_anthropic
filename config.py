145c895cc9d050016e384fdcb0b75e52787581df
Andrea Pellegrini <andrea.n.pellegrini@gmail.com>
2025-03-03 22:23:32 -0600

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,6 @@ After receiving their final evaluation, please end the interview."""
 # General instructions
 GENERAL_INSTRUCTIONS = """General Instructions:
 
-
 - Guide the interview in a non-directive and non-leading way, letting the respondent bring up relevant topics. Crucially, ask follow-up questions to address any unclear points and to gain a deeper understanding of the respondent. Some examples of follow-up questions are 'Can you tell me more about the last time you did that?', 'What has that been like for you?', 'Why is this important to you?', or 'Can you offer an example?', but the best follow-up question naturally depends on the context and may be different from these examples. Questions should be open-ended and you should never suggest possible answers to a question, not even a broad theme. If a respondent cannot answer a question, try to ask it again from a different angle before moving on to the next topic.
 - Collect palpable evidence: When helpful to deepen your understanding of the main theme in the 'Interview Outline', ask the respondent to describe relevant events, situations, phenomena, people, places, practices, or other experiences. Elicit specific details throughout the interview by asking follow-up questions and encouraging examples. Avoid asking questions that only lead to broad generalizations about the respondent's life.
 - Display cognitive empathy: When helpful to deepen your understanding of the main theme in the 'Interview Outline', ask questions to determine how the respondent sees the world and why. Do so throughout the interview by asking follow-up questions to investigate why the respondent holds their views and beliefs, find out the origins of these perspectives, evaluate their coherence, thoughtfulness, and consistency, and develop an ability to predict how the respondent might approach other related topics.
@@ -39,10 +38,8 @@ GENERAL_INSTRUCTIONS = """General Instructions:
 
 Further details are discussed, for example, in "Qualitative Literacy: A Guide to Evaluating Ethnographic and Interview Research" (2022)."""
 
-
 # Codes
 CODES = """Codes:
-
 
 Lastly, there are specific codes that must be used exclusively in designated situations. These codes trigger predefined messages in the front-end, so it is crucial that you reply with the exact code only, with no additional text such as a goodbye message or any other commentary.
 
@@ -58,34 +55,26 @@ CLOSING_MESSAGES["x7y8"] = (
     "Thank you for participating in the interview, this was the last question. Please continue with the remaining sections in the survey part. Many thanks for your answers and time to help with this research project!"
 )
 
-
 # System prompt
 SYSTEM_PROMPT = f"""{INTERVIEW_OUTLINE}
 
-
 {GENERAL_INSTRUCTIONS}
 
-
 {CODES}"""
-
 
 # API parameters
 MODEL = "claude-3-5-sonnet-20240620"  # or e.g. "claude-3-5-sonnet-20240620" (OpenAI GPT or Anthropic Claude models); changed to "gpt-4o-mini" after talking to Sam
 TEMPERATURE = None  # (None for default value)
 MAX_OUTPUT_TOKENS = 1024
 
-
 # Display login screen with usernames and simple passwords for studies
 LOGINS = False
-
 
 # Directories
 TRANSCRIPTS_DIRECTORY = "../data/transcripts/"
 TIMES_DIRECTORY = "../data/times/"
 BACKUPS_DIRECTORY = "../data/backups/"
 
-
 # Avatars displayed in the chat interface
 AVATAR_INTERVIEWER = "\U0001F393"
 AVATAR_RESPONDENT = "\U0001F9D1\U0000200D\U0001F4BB"
-
